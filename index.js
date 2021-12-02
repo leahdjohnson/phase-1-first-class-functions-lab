435bea42bb2002e8df142a5eb1b6b5a830b88ffa
Leah <elloworldt@gmail.com>
2021-12-01 16:43:14 -0800

    commit tree 9d42943588e8cf4e59dc137ba6dcc031a74fc2c0

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (drivers, returnFirstTwoDrivers) {
+    return drivers.filter([0, 1])
+};
+
+const _returnLastTwoDrivers = function (drivers, returnLastTwoDrivers) {
+    return returnLastTwoDrivers();
+};
+
+
+const selectingDrivers = [returnFirstTwoDrivers(), returnLastTwoDrivers()];
+
+function createFareMultiplier(integer) {
+    return integer * 4
+}
+
+function fareDoubler(createFareMultiplier) {
+    return createFareMultiplier() * 2    
+}
+
+function fareTripler(createFareMultiplier){
+return createFareMultiplier() * 2     
+}
+
+function fareQuintupler(createFareMultiplier){
+    return createFareMultiplier() * 2     
+    }
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers, returnLastTwoDrivers) {
+return 
+}
